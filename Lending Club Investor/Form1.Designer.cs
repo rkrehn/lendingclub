a969f6f83ca74c21e1aa106603e5186b0c3e7cf9
rkrehn <rV4P0nXQKy17es0k>
2015-10-20 14:20:24 -0600

    First edition

---
+++ Lending Club Investor/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Lending_Club_Investor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txt_Cash = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(100, 103);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Submit order";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Summary";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txt_Cash
+            // 
+            this.txt_Cash.AutoSize = true;
+            this.txt_Cash.Location = new System.Drawing.Point(97, 22);
+            this.txt_Cash.Name = "txt_Cash";
+            this.txt_Cash.Size = new System.Drawing.Size(35, 13);
+            this.txt_Cash.TabIndex = 2;
+            this.txt_Cash.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(260, 154);
+            this.Controls.Add(this.txt_Cash);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label txt_Cash;
+    }
+}
+
---
+++ Lending Club Investor/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+
+
+// API key: jzWKvgAd5CfhictJtZzMpYEaNRI=
+// account number: 1633758
+
+// webby: https://www.lendingclub.com/developers/submit-order.action
+
+namespace Lending_Club_Investor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var httpWebRequest = (HttpWebRequest)WebRequest.Create("https://api.lendingclub.com/api/investor/v1/accounts/1633758/orders");
+            httpWebRequest.ContentType = "text/json";
+            httpWebRequest.Method = "POST";
+
+            using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+            {
+                string json = "{\"user\":\"test\"," +
+                              "\"password\":\"bla\"}";
+
+                streamWriter.Write(json);
+                streamWriter.Flush();
+                streamWriter.Close();
+            }
+
+            var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            {
+                var result = streamReader.ReadToEnd();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // build variables
+            var url = "https://api.lendingclub.com/api/investor/v1/accounts/1633758/summary";
+
+            // authentication, contenttype, etc. per API
+            var webrequest = (HttpWebRequest)WebRequest.Create(url);
+            webrequest.Method = "GET";
+            webrequest.ContentType = "application/json";
+            webrequest.UserAgent = "Mozilla/5.0 (Windows NT 5.1; rv:28.0) Gecko/20100101 Firefox/28.0";
+            webrequest.Headers.Add("Authorization", "jzWKvgAd5CfhictJtZzMpYEaNRI=");
+
+            // Setup download
+            WebResponse response = webrequest.GetResponse();
+            Stream datastream = response.GetResponseStream();
+            StreamReader sr = new StreamReader(datastream);
+
+            // read data
+            string json = sr.ReadToEnd();
+
+            int x = json.IndexOf("availableCash", 0);
+            int y = json.IndexOf(",", x);
+
+            json = json.Substring(x + 15, y - (x + 15));
+
+            decimal calc = Convert.ToDecimal(json);
+            decimal calc2 = Math.Round(calc / 25)*25;
+            if (calc2 > calc) calc2 =- 25;
+
+            txt_Cash.Text = calc2.ToString();
+
+            // display available cashs
+            // "availableCash\":
+            // ,
+        }
+    }
+}
